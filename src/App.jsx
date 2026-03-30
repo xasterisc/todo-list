55f40b64eb86422b0fed2c1bc0d5f44458a9d027
Mohamed Zouari <62741160+xasterisc@users.noreply.github.com>
2026-03-30 23:29:21 +0100

    feat: add first Todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import './App.css'
+import './App.css';
 
 function App() {
+  const todoList = [
+    { id: 1, title: 'review resources' },
+    { id: 2, title: 'take notes' },
+    { id: 3, title: 'code out app' },
+  ];
 
   return (
-   <div>
-    <h1>My Todos</h1>
-   </div>
-  )
+    <div>
+      <h1>My Todos</h1>
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
