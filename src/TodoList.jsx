0343784b9a5506e50688f4e0cf2381f786bcf391
Mohamed Zouari <62741160+xasterisc@users.noreply.github.com>
2026-04-13 22:47:27 +0100

    Create the TodoList Component

---
+++ src/TodoList.jsx
@@ -0,0 +1,17 @@
+const TodoList = () => {
+  const todoList = [
+    { id: 1, title: 'review resources' },
+    { id: 2, title: 'take notes' },
+    { id: 3, title: 'code out app' },
+  ];
+
+  return (
+    <ul>
+      {todoList.map((todo) => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
