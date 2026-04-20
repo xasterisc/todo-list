d86a41304b34615f79bace0983973ae8cb42f4ff
Mohamed Zouari <62741160+xasterisc@users.noreply.github.com>
2026-04-20 23:57:36 +0100

    Implement useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import './App.css';
 import TodoList from './TodoList.jsx';
 import TodoForm from './TodoForm.jsx';
+import { useState } from 'react';
+
+const todos = [
+  { id: 1, title: 'review resources' },
+  { id: 2, title: 'take notes' },
+  { id: 3, title: 'code out app' },
+];
 
 function App() {
+  const [todoList, setTodoList] = useState(todos);
+
   return (
     <div>
       <h1>My Todos</h1>
