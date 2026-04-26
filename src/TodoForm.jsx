5435d1676b03533cee0a3ebaed82ef984316ec28
Mohamed Zouari <62741160+xasterisc@users.noreply.github.com>
2026-04-26 22:31:11 +0100

    Create Form Submit Handler

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,9 +1,35 @@
-const TodoForm = () => {
+import { useRef } from 'react';
+
+const TodoForm = ({ onAddTodo }) => {
+  const inputRef = useRef(null);
+
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+
+    console.log('Event object:', event);
+    console.log('Event target:', event.target);
+    console.log('Input value:', event.target.form.todoTitle.value);
+
+    const todoTitle = event.target.form.todoTitle.value.trim();
+    if (todoTitle && todoTitle !== '') {
+      onAddTodo(todoTitle);
+      event.target.form.reset();
+      inputRef.current.focus();
+    }
+  };
+
   return (
     <form>
       <label htmlFor='todoTitle'>Todo</label>
-      <input type='text' id='todoTitle' />
-      <button type='submit' disabled>
+      <input
+        ref={inputRef}
+        type='text'
+        id='todoTitle'
+        name='todoTitle'
+        placeholder={'Todo text'}
+        required
+      />
+      <button type='submit' onClick={handleAddTodo}>
         Add Todo
       </button>
     </form>
