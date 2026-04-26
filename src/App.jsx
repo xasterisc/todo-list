84beae50b50a20fcde4c678fe71d42a554e38ced
Mohamed Zouari <62741160+xasterisc@users.noreply.github.com>
2026-04-26 21:55:57 +0100

    Update Initial State

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,8 @@ import TodoList from './TodoList.jsx';
 import TodoForm from './TodoForm.jsx';
 import { useState } from 'react';
 
-const todos = [
-  { id: 1, title: 'review resources' },
-  { id: 2, title: 'take notes' },
-  { id: 3, title: 'code out app' },
-];
-
 function App() {
-  const [todoList, setTodoList] = useState(todos);
+  const [todoList, setTodoList] = useState([]);
 
   return (
     <div>
