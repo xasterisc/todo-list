64e0f9af83ee698361fd001c0c90f3ac546993f1
Mohamed Zouari <62741160+xasterisc@users.noreply.github.com>
2026-05-10 22:15:12 +0100

    Refactor Input and Label into a Re-usable Component

---
+++ src/shared/TextInputWithLabel.jsx
@@ -0,0 +1,10 @@
+const TextInputWithLabel = () => {
+  return (
+    <>
+      <label htmlFor='TextInputWithLabel'></label>
+      <input type='text' id='TextInputWithLabel' />
+    </>
+  );
+};
+
+export default TextInputWithLabel;
