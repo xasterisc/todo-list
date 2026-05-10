6ddf82ca357aaf20c8189ac5617536e775e21aab
Mohamed Zouari <62741160+xasterisc@users.noreply.github.com>
2026-05-10 22:10:27 +0100

    Re-organize Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
-import TodoList from './TodoList.jsx';
-import TodoForm from './TodoForm.jsx';
+import TodoList from './features/TodoList/TodoList.jsx';
+import TodoForm from './features/TodoForm.jsx';
 import { useState } from 'react';
 
 function App() {
