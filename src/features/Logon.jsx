f39f096cf8b79be0ef7d635aca271fff1f6a0685
perlarose <107357329+perlarose@users.noreply.github.com>
2026-05-18 22:30:57 +0100

    Create the Logon Component with Pessimistic UI update

---
+++ src/features/Logon.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+
+const Logon = ({ onSetEmail = () => {}, onSetToken = () => {} }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [authError, setAuthError] = useState('');
+  const [isLoggingOn, setIsLoggingOn] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefaul();
+    setIsLoggingOn(true);
+    try {
+      const response = await fetch('/api/users/logon', {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        credentials: 'include',
+        body: JSON.stringify({ email, password }),
+      });
+      const data = await response.json();
+      console.dir(response);
+      if (response.status === 200 && data.name && data.csrfToken) {
+        onSetEmail(data.name);
+        onSetToken(data.csrfToken);
+      } else {
+        setAuthError(`Authentication failed: ${data?.message}`);
+      }
+    } catch (error) {
+      setAuthError(`Error: ${error.name} | ${error.message}`);
+    } finally {
+      setIsLoggingOn(false);
+    }
+  };
+
+  return (
+    <>
+      {authError && <p>{authError}</p>}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='email'>email</label>
+        <input
+          type='email'
+          name='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label htmlFor='password'>password</label>
+        <input
+          type='password'
+          name='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type='submit' disabled={isLoggingOn}>
+          {isLoggingOn ? 'Logging in...' : 'Log On'}
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default Logon;
