caa46bee3af15bde2c800125c1e6e26a6c78f417
Mohamed Zouari <62741160+xasterisc@users.noreply.github.com>
2026-05-28 22:17:56 +0100

    Create useDebounce Hook

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => clearTimeout(timeoutId);
+  }, [delay, value]);
+
+  return debouncedValue;
+};
+
+export { useDebounce };
