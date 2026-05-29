ca5029586ac4beeacfe92baf93f4234de15e78cc
Mohamed Zouari <62741160+xasterisc@users.noreply.github.com>
2026-05-29 12:02:59 +0100

    Remove all console.log statements

--- src/features/Todos/TodoList/TodoList.jsx
+++ src/features/Todos/TodoList/TodoList.jsx
@@ -3,7 +3,6 @@ import TodoListItem from './TodoListItem';
 
 const TodoList = ({ dataVersion, onCompleteTodo, onUpdateTodo, todoList }) => {
   const filteredTodoList = useMemo(() => {
-    console.log(`Recalculating filtered todos (${dataVersion})`);
     return {
       version: dataVersion,
       todos: todoList.filter((todo) => !todo.isCompleted),
