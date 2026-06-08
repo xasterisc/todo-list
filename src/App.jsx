28d63cf10dd2258a8b232d9b0fa8b73b7891c396
Mohamed Zouari <62741160+xasterisc@users.noreply.github.com>
2026-06-08 21:22:14 +0100

    Configure All Routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
 import './App.css';
-import TodosPage from './pages/TodosPage';
 import Header from './shared/Header';
-import Logon from './features/Logon';
+import TodosPage from './pages/TodosPage';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import LoginPage from './pages/LoginPage';
+import ProfilePage from './pages/ProfilePage';
+import NotFoundPage from './pages/NotFoundPage';
+import RequireAuth from './components/RequireAuth';
 import { Routes, Route } from 'react-router';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <Routes>
-        <Route />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route
+          path='/todos'
+          element={
+            <RequireAuth>
+              <TodosPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path='/profile'
+          element={
+            <RequireAuth>
+              <ProfilePage />
+            </RequireAuth>
+          }
+        />
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
